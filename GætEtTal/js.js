447e9d1e92b040c0ccf3e1bb39fa07ba6558cf71
andreadamgaard <124664759+andreadamgaard@users.noreply.github.com>
2024-01-30 23:21:39 +0100

    Gæt et tal + animation

---
+++ GætEtTal/js.js
@@ -0,0 +1,36 @@
+"use strict";
+// nummeret er oprettet og kan bruges i spillet
+const btn = document.querySelector("#btn");
+const userInput = document.querySelector("#input");
+const feedbackSlot = document.querySelector("#feedback");
+const celebrationElement = document.querySelector("#tillykke");
+
+// funktion til random tal
+function generateRandomNum(maxValue) {
+  return Math.floor(Math.random() * maxValue);
+}
+
+let randNum = generateRandomNum(100);
+console.log(randNum);
+
+function feedback(value, input) {
+  if (value === input) {
+    feedbackSlot.innerHTML = "Okaay, tillykke!! Du har gættet rigtigt!";
+    celebrationElement.style.display = "block";
+  }
+  if (value > input) {
+    feedbackSlot.innerHTML = "Du har gættet for lavt :(";
+  }
+  if (value < input) {
+    feedbackSlot.innerHTML = "Du har gættet for højt :(";
+  }
+  if (input > 100) {
+    feedbackSlot.innerHTML = "Kun tal mellem 0-100 din taber :(";
+  }
+}
+
+btn.addEventListener("click", () => {
+  userInput.classList = "";
+  const input = parseInt(userInput.value);
+  feedback(randNum, input);
+});
