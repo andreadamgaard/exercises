b545493ed1ccc73f5bc9f938fde1bc05ac84f0e9
andreadamgaard <124664759+andreadamgaard@users.noreply.github.com>
2024-02-08 09:29:22 +0100

    bar chart

--- bar_chart_starter_kit/script.js
+++ bar_chart_starter_kit/script.js
@@ -1,5 +1,26 @@
 const list = document.querySelector("ul");
-
+// Laver en søjle der er li og er lagt i ul liste.
 const li = document.createElement("li");
-li.style.setProperty("--height", "30");
-list.appendChild(li);
+const valueArr = [];
+
+setInterval(generersojle, 1000);
+
+function generersojle() {
+  console.log(valueArr);
+  valueArr.push(getRandomNum());
+
+  const randoNum = getRandomNum();
+  const li = document.createElement("li");
+  //Sætter liste højden til random højde.
+  li.style.setProperty("--height", randoNum);
+  list.appendChild(li);
+
+  if (valueArr.length > 20) {
+    valueArr.shift();
+    list.removeChild(list.firstChild);
+  }
+}
+
+function getRandomNum() {
+  return Math.floor(Math.random() * 100);
+}
--- breadcrumb/js.js
+++ breadcrumb/js.js
@@ -5,10 +5,22 @@ const bc = [
   { name: "Vaskemaskiner", link: "/hvidevarer/vaskemaskiner" },
   { name: "Bosch", link: "/hvidevarer/vaskemaskiner/bosch/" },
 ];
+document.querySelector("button").addEventListener("click", generateBreadCrumb);
+const pPointer = document.querySelector("p");
 
-//Func der sender vores links til HTML som a href elementer
-
-//Func der returnere navne på hvert brødkrumme element
-//Sidste punkt skal kun være tekst
-
-//Lav itteraktioner med knappen så de skifter
+function generateBreadCrumb() {
+  bc.forEach((crumb, i) => {
+    //Iteratoren der burges til at finde længden af vores array
+    console.log("i", i);
+    //Her siger vi til den at i den sidste string skal den gøre noget andet
+    //Her fx at den kun skal vise navnet, men ikke linket.
+    if (i === bc.length - 1) {
+      //console.log("Det er det sidsete element der hedder ", crumb.name);
+      pPointer.innerHTML += crumb.name;
+    } else {
+      //viser vores bredcrumb links, med rigtige links.
+      pPointer.innerHTML += `<a href=${crumb.link}>${crumb.name}</a> / `;
+      //Mellemrum efter </a> gør der kommer mellemrum i vores array udskriving
+    }
+  });
+}
